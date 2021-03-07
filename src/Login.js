ba90e868e20cc33aca03c50abde3c92df0d46871
Abhishek EH <collegewap@gmail.com>
2021-03-07 17:44:38 +0530

    Login and Registratio form submission

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,53 @@
-import { Button, FormGroup, InputGroup } from "@blueprintjs/core";
-import React, { useState } from "react";
+import { Button, Callout, FormGroup, InputGroup } from "@blueprintjs/core";
+import React, { useContext, useState } from "react";
+import { UserContext } from "./context/UserContext";
 
 const Login = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [userContext, setUserContext] = useContext(UserContext);
 
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    setError("");
+
+    const genericErrorMessage = "Something went wrong! Please try again later.";
+
+    fetch(process.env.REACT_APP_API_ENDPOINT + "users/login", {
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username: email, password }),
+    })
+      .then(async (response) => {
+        setIsSubmitting(false);
+        if (!response.ok) {
+          if (response.status === 400) {
+            setError("Please fill all the fields correctly!");
+          } else if (response.status === 401) {
+            setError("Invalid email and password combination.");
+          } else {
+            setError(genericErrorMessage);
+          }
+        } else {
+          const data = await response.json();
+          setUserContext((oldValues) => {
+            return { ...oldValues, token: data.token };
+          });
+        }
+      })
+      .catch((error) => {
+        setIsSubmitting(false);
+        setError(genericErrorMessage);
+      });
+  };
   return (
     <>
-      <form className="auth-form">
+      {error && <Callout intent="danger">{error}</Callout>}
+      <form onSubmit={formSubmitHandler} className="auth-form">
         <FormGroup label="Email" labelFor="email">
           <InputGroup
             id="email"
@@ -26,7 +66,13 @@ const Login = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
         </FormGroup>
-        <Button intent="primary" fill type="submit" text="Sign In" />
+        <Button
+          intent="primary"
+          disabled={isSubmitting}
+          text={`${isSubmitting ? "Signing In" : "Sign In"}`}
+          fill
+          type="submit"
+        />
       </form>
     </>
   );
--- src/Register.js
+++ src/Register.js
@@ -1,21 +1,66 @@
-import { Button, FormGroup, InputGroup } from "@blueprintjs/core";
-import React, { useState } from "react";
+import { Button, Callout, FormGroup, InputGroup } from "@blueprintjs/core";
+import React, { useContext, useState } from "react";
+import { UserContext } from "./context/UserContext";
 
 const Register = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState("");
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [userContext, setUserContext] = useContext(UserContext);
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    setError("");
+
+    const genericErrorMessage = "Something went wrong! Please try again later.";
+
+    fetch(process.env.REACT_APP_API_ENDPOINT + "users/signup", {
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ firstName, lastName, username: email, password }),
+    })
+      .then(async (response) => {
+        setIsSubmitting(false);
+        if (!response.ok) {
+          if (response.status === 400) {
+            setError("Please fill all the fields correctly!");
+          } else if (response.status === 401) {
+            setError("Invalid email and password combination.");
+          } else if (response.status === 500) {
+            console.log(response);
+            const data = await response.json();
+            if (data.message) setError(data.message || genericErrorMessage);
+          } else {
+            setError(genericErrorMessage);
+          }
+        } else {
+          const data = await response.json();
+          setUserContext((oldValues) => {
+            return { ...oldValues, token: data.token };
+          });
+        }
+      })
+      .catch((error) => {
+        setIsSubmitting(false);
+        setError(genericErrorMessage);
+      });
+  };
 
   return (
     <>
-      <form className="auth-form">
+      {error && <Callout intent="danger">{error}</Callout>}
+
+      <form onSubmit={formSubmitHandler} className="auth-form">
         <FormGroup label="First Name" labelFor="firstName">
           <InputGroup
             id="firstName"
             placeholder="First Name"
             onChange={(e) => setFirstName(e.target.value)}
-            value={firstName}
           />
         </FormGroup>
         <FormGroup label="Last Name" labelFor="firstName">
@@ -23,7 +68,6 @@ const Register = () => {
             id="lastName"
             placeholder="Last Name"
             onChange={(e) => setLastName(e.target.value)}
-            value={lastName}
           />
         </FormGroup>
         <FormGroup label="Email" labelFor="email">
@@ -32,7 +76,6 @@ const Register = () => {
             type="email"
             placeholder="Email"
             onChange={(e) => setEmail(e.target.value)}
-            value={email}
           />
         </FormGroup>
         <FormGroup label="Password" labelFor="password">
@@ -41,10 +84,15 @@ const Register = () => {
             placeholder="Password"
             type="password"
             onChange={(e) => setPassword(e.target.value)}
-            value={password}
           />
         </FormGroup>
-        <Button intent="primary" text="Register" fill type="submit" />
+        <Button
+          intent="primary"
+          disabled={isSubmitting}
+          text={`${isSubmitting ? "Registering" : "Register"}`}
+          fill
+          type="submit"
+        />
       </form>
     </>
   );
